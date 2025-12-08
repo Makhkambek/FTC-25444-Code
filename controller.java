c61bf2f7732b3f0afc19a1dfcb8932731d6f2278
Javokhir <aaplle765@gmail.com>
2025-12-08 23:59:18 +0500

    Drivetrain.java

--- controller.java
+++ controller.java
@@ -1,32 +1,34 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.*;
 
-public class Drivetrain  {
-    private final DcMotor leftFront, leftBack, rightFront, rightBack;
-    public DriveSubsystem(HardwareMap hw) {
-        leftFront  = hw.dcMotor.get("leftFront");
-        leftBack   = hw.dcMotor.get("leftBack");
-        rightFront = hw.dcMotor.get("rightFront");
-        rightBack  = hw.dcMotor.get("rightBack");
+public class Drivetrain {
+
+    private DcMotor leftFront, leftBack, rightFront, rightBack;
+
+    public Drivetrain(HardwareMap hw) {
+        leftFront  = hw.get(DcMotor.class,"leftFront");
+        leftBack   = hw.get(DcMotor.class,"leftBack");
+        rightFront = hw.get(DcMotor.class,"rightFront");
+        rightBack  = hw.get(DcMotor.class,"rightBack");
 
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
     }
-    public void drive (double drive, double turn, boolean slowmode){
-    double leftPower = drive -turn;
-    double rightPower = drive +turn;
 
-    if (slowmode){
-        leftPower *= 0.5;
-        rightPower *= 0.5;
+    public void drive(double drive, double turn, boolean slowmode) {
+
+        double leftPower = drive - turn;
+        double rightPower = drive + turn;
+
+        if (slowmode) {
+            leftPower *= 0.5;
+            rightPower *= 0.5;
+        }
+
+        leftFront.setPower(leftPower);
+        leftBack.setPower(leftPower);
+        rightFront.setPower(rightPower);
+        rightBack.setPower(rightPower);
     }
-    leftFront.setPower(leftPower);
-    leftBack.setPower(leftPower);
-    rightFront.setPower(rightPower);
-    rightBack.setPower(rightPower);
-    }
-  //  @Override public void periodic() {}
 }
