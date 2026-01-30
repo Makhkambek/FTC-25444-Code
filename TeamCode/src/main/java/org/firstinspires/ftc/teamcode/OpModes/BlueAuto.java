4655f6015d7d309173493d3de6c70f4e789adeff
Makhkambek <okeydokeykokki@gmail.com>
2026-01-30 23:02:03 +0500

    auto 9

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/BlueAuto.java
@@ -22,7 +22,7 @@ public class BlueAuto extends OpMode {
     private Timer pathTimer;
     private int pathState = 0;
 
-    private PathChain path1, path2, path3, path4;
+    private PathChain path1, path2, path3, path4, path5;
     private final Pose startPose = new Pose(39.945, 135.779, Math.toRadians(270));
 
 //    private Intake intake;
@@ -36,8 +36,9 @@ public class BlueAuto extends OpMode {
         path1 = follower.pathBuilder()
                 .addPath(
                         new BezierLine(
-                                new Pose(39.945, 135.779),
-                                new Pose(60.000, 84.000)
+                                new Pose(39.539, 134.763),
+
+                                new Pose(61.422, 93.343)
                         )
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(180))
@@ -47,9 +48,9 @@ public class BlueAuto extends OpMode {
         path2 = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Pose(60.000, 84.000),
-                                new Pose(55.034, 55.046),
-                                new Pose(16.262, 58.841)
+                                new Pose(61.422, 93.343),
+                                new Pose(60.665, 60.894),
+                                new Pose(27.787, 60.035)
                         )
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(180))
@@ -59,9 +60,9 @@ public class BlueAuto extends OpMode {
         path3 = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Pose(16.262, 58.841),
-                                new Pose(62.117, 66.660),
-                                new Pose(60.000, 84.000)
+                                new Pose(27.787, 60.035),
+                                new Pose(50.979, 68.446),
+                                new Pose(61.412, 93.322)
                         )
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(150))
@@ -71,9 +72,20 @@ public class BlueAuto extends OpMode {
         path4 = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Pose(60.000, 84.000),
-                                new Pose(61.915, 66.612),
-                                new Pose(16.503, 58.841)
+                                new Pose(61.532, 93.316),
+                                new Pose(62.561, 64.384),
+                                new Pose(23.924, 74.213)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(150), Math.toRadians(150))
+                .build();
+
+        path5 = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(23.924, 74.213),
+                                new Pose(61.745, 63.821),
+                                new Pose(61.525, 93.268)
                         )
                 )
                 .setLinearHeadingInterpolation(Math.toRadians(150), Math.toRadians(150))
@@ -91,7 +103,7 @@ public class BlueAuto extends OpMode {
             case 1: // Ожидание завершения Path 1
                 if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() >= 0.1) {
 //                    shooter.startShoot();
-                    setPathState(25);
+                    setPathState(2);
                 }
                 break;
 
@@ -113,7 +125,7 @@ public class BlueAuto extends OpMode {
                 break;
 
             case 5: // Ожидание завершения Path 3
-                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() >= 0.1) {
+                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() >= 0.5) {
                     setPathState(6);
                 }
                 break;
@@ -132,9 +144,9 @@ public class BlueAuto extends OpMode {
                 break;
 
             case 8: // Ожидание завершения Path 4 - завершение
-                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() >= 0.1) {
+                if (!follower.isBusy() && pathTimer.getElapsedTimeSeconds() >= 2.5) {
 //                    intake.off();
-
+                        follower.followPath(path5, true);
                     // Сохраняем финальную позицию робота для передачи в TeleOp
                     // Измените на фактические координаты конца автономки
 //                    localizer.setPosition(16.503, 58.841, 150);
